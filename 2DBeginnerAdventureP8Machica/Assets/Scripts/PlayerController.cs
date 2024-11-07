aad77f250f4c4094c052c1301c255aef29686c84
swagdog13 <727159@student.dusd.net>
2024-11-07 15:22:20 -0800

    I worked on a script in my school project
    I worked on the damage zone script for damage zone objects.

--- 2DBeginnerAdventureP8Machica/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Machica/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -19,27 +23,46 @@ public class PlayerController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         horizontal = Input.GetAxis("Horizontal");
-         vertical = Input.GetAxis("Vertical");
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+
+        }
     }
-    void FixedUpdate()
-    {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
-        
-        rigidbody2d.MovePosition(position);
-    }
-    public void ChangeHealth(int amount)
-    {
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
-    }
+        void FixedUpdate()
+        {
+            Vector2 position = rigidbody2d.position;
+            position.x = position.x + speed * horizontal * Time.deltaTime;
+            position.y = position.y + speed * vertical * Time.deltaTime;
+
+            rigidbody2d.MovePosition(position);
+        }
+        public void ChangeHealth(int amount)
+        {
+            if (amount < 0)
+            {
+                if (isInvincible)
+                {
+                    return;
+                }
+                isInvincible = true;
+                invincibleTimer = timeInvincible;
+            }
+            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+            Debug.Log(currentHealth + "/" + maxHealth);
+        }
 }
