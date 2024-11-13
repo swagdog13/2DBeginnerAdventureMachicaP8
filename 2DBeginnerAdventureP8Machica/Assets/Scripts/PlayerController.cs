39981417ac6f4b57b07fe791cf5a0986b6d493ad
swagdog13 <727159@student.dusd.net>
2024-11-12 22:21:43 -0800

    added animations to ducko
    I added walking animations to my main character Ducko.

--- 2DBeginnerAdventureP8Machica/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Machica/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -18,11 +18,15 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
 
     }
@@ -32,6 +36,17 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) ||!Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -55,6 +70,8 @@ public class PlayerController : MonoBehaviour
         {
             if (amount < 0)
             {
+            animator.SetTrigger("Hit");
+
                 if (isInvincible)
                 {
                     return;
