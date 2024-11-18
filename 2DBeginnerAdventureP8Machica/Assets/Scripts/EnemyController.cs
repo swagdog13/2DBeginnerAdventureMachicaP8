ea5423c898f9c4c2c8d4b53be54f7a051b959603
swagdog13 <727159@student.dusd.net>
2024-11-18 15:25:48 -0800

    UIHealthBar and particles
    I added a healthbar to my main character. I also added smoke to come out of my enemies

--- 2DBeginnerAdventureP8Machica/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Machica/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+    
     Rigidbody2D rigidbody2d;
 
     bool broken = true;
@@ -79,5 +81,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
